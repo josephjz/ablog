036f983834d39a4e2862a7d629393796d56d0d75
Jennifer Joseph <josephjz@bc.edu>
2021-04-23 11:47:15 -0400

    video #1 done

--- tutorialblog/admin.py
+++ tutorialblog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# have to register any new models in our admin area 
+
+admin.site.register(Post) # this allows our blog post entries to be accessible from blog post area, creates the Posts table on admin
--- tutorialblog/models.py
+++ tutorialblog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    author = models.ForeignKey(User, on_delete = models.CASCADE) # deletes blog post from a user that has been deleted
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)   #lets us see title and author on the admin back end 
+
