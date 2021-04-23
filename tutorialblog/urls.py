7159cc36192a5b00cf108a5f52a6b361108949e3
Jennifer Joseph <josephjz@bc.edu>
2021-04-23 11:23:06 -0400

    templates folder created for all new html templates to be stored

---
+++ tutorialblog/urls.py
@@ -0,0 +1,26 @@
+"""ablog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# copied and pasted the code written in project wide urls.py, created a new app urls.py file, and pasted the code here 
+
+# deleted admin import 
+from django.urls import path  # deleted include import
+from . import views
+
+urlpatterns = [
+    # deleted paths from project wide urls.py, instead writing new ones 
+    path('', views.home, name = "home")  # this name is the function name in the views.py file, which we need to go create in urls.py
+]
