e4dc9a096b10043254894c80cc2fd26e554023cd
Jennifer Joseph <josephjz@bc.edu>
2021-04-23 11:09:39 -0400

    added the project app to the installed apps of project wide settings.py

--- ablog/settings.py
+++ ablog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tutorialblog', # added the project app to the installed apps of project wide settings.py
 ]
 
 MIDDLEWARE = [
