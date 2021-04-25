7f120f26966d80a97031c637bbb3fe574cce02b9
Jennifer Joseph <josephjz@bc.edu>
2021-04-25 11:24:48 -0400

    video 3

--- tutorialblog/models.py
+++ tutorialblog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length = 255)
+    title_tag = models.CharField(max_length = 255, default = "My Freaking Awesome Blog")
     author = models.ForeignKey(User, on_delete = models.CASCADE) # deletes blog post from a user that has been deleted
                                                                 # think about adding this for Biblio
     # ForeignKey key is from the User 
