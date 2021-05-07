d9414c533e6ddf88f77b9684c700974aa15c6771
Jennifer Joseph <josephjz@bc.edu>
2021-05-07 16:27:41 -0400

    video 33 done

--- tutorialblog/admin.py
+++ tutorialblog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Category, Profile
+from .models import Post, Category, Profile, Comment
 # have to register any new models in our admin area 
 
 admin.site.register(Post) # this allows our blog post entries to be accessible from blog post area, creates the Posts table on admin
 admin.site.register(Category)
 admin.site.register(Profile)
+admin.site.register(Comment)
--- tutorialblog/models.py
+++ tutorialblog/models.py
@@ -29,7 +29,7 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user)
-        
+
     def get_absolute_url(self):
         return reverse('home')
 
@@ -65,3 +65,18 @@ class Post(models.Model):
         # this will take it to the post that was just created!
         # want to point this back to the articles page 
         # any time seomthing is created in our model, it gets an id (kinda the same thing as primary key )
+
+
+# commenting on the blog posts so need to associate this model with Post 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name = "comments", on_delete = models.CASCADE) # foreign key 
+    name = models.CharField(max_length = 255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name) # admin area will show post title that comment is on, and who commented 
+
+
+
